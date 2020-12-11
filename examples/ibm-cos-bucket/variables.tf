1180633b814218b9a02971e2f2792b6d23a219d7
Crystal Barragan <crystal.barragan@ibm.com>
2020-12-10 19:28:32 -0600

    Update variables.tf

--- examples/ibm-cos-bucket/variables.tf
+++ examples/ibm-cos-bucket/variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
-  default = "a-standard-bucket-at-ams-firewall"
+  default = "tutorial-bucket"
 }
 
 variable "resource_group_name" {
@@ -14,30 +14,30 @@ variable "region" {
   default = "us"
 }
 
-variable "archive_ruleid" {
-  default = ""
-}
+#variable "archive_ruleid" {
+#  default = ""
+#}
 
 variable "regional_loc" {
   default = "us-south"
 }
 
-variable "archive_days" {
-  default = 0
-}
+#variable "archive_days" {
+#  default = 0
+#}
 
-variable "archive_types" {
-  default = "ACCELERATED"
-}
+#variable "archive_types" {
+#  default = "ACCELERATED"
+#}
 
-variable "expire_ruleid" {
-  default = ""
-}
+#variable "expire_ruleid" {
+#  default = ""
+#}
 
-variable "expire_days" {
-  default = 1
-}
+#variable "expire_days" {
+#  default = 1
+#}
 
-variable "expire_prefix" {
-  default = ""
-}
+#variable "expire_prefix" {
+#  default = ""
+#}
