d9473ac00a82776fef6073b370da878a060ff601
Crystal Barragan <crystal.barragan@ibm.com>
2020-12-10 19:29:56 -0600

    Update variables.tf

--- examples/ibm-cos-bucket/variables.tf
+++ examples/ibm-cos-bucket/variables.tf
@@ -14,30 +14,6 @@ variable "region" {
   default = "us"
 }
 
-#variable "archive_ruleid" {
-#  default = ""
-#}
-
 variable "regional_loc" {
   default = "us-south"
 }
-
-#variable "archive_days" {
-#  default = 0
-#}
-
-#variable "archive_types" {
-#  default = "ACCELERATED"
-#}
-
-#variable "expire_ruleid" {
-#  default = ""
-#}
-
-#variable "expire_days" {
-#  default = 1
-#}
-
-#variable "expire_prefix" {
-#  default = ""
-#}
